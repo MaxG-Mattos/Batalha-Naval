30faeb5a661c729787f49b6da7bc683d184574d3
MaxG-Mattos <166611554+MaxG-Mattos@users.noreply.github.com>
2024-04-15 10:31:26 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
-letras = ['A','B','C','D','E','F','G','H','I','J']
-posicoes = [10*letras]
+letras = [[],[],[],[],[],[],[],[],[],[]]
+posicoes = []
+i = 1
+while i <=10:
+    posicoes.append(letras)
+    i+=1
+# eu tava pensando que daria para fazer algo assim e considerar as linhas como as letras
+# e os numeros como colunas.
+# pensando se fazer os comandos como dicionario é uma boa ideia...
